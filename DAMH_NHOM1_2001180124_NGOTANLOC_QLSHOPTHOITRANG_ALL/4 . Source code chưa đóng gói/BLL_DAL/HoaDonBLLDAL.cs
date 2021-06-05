abe513eb0edf1ab7101215b980169935ac6f466e
ngoloc184 <ngoloc135@gmail.com>
2021-06-06 01:00:26 +0700

    sadsad

---
+++ DAMH_NHOM1_2001180124_NGOTANLOC_QLSHOPTHOITRANG_ALL/4 . Source code chưa đóng gói/BLL_DAL/HoaDonBLLDAL.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_DAL
+{
+    public class HoaDonBLLDAL
+    {
+        QLShopDataContext dbContext = new QLShopDataContext();
+        public List<vw_HoaDon> loadHoaDonTheoNgay()
+        {
+            List<vw_HoaDon> list = new List<vw_HoaDon>();
+            foreach (vw_HoaDon item in dbContext.vw_HoaDons) {
+                if (item.NGAYTAO.Value.Date.Day == DateTime.Now.Date.Day) {
+                    if (item.TENKHACHHANG == null)
+                        item.TENKHACHHANG = "Khách hàng vãng lai";
+                    list.Add(item);
+                }
+            }
+            return list;
+            //return dbContext.vw_HoaDons.Where(hd => hd.NGAYTAO.Value.Date.Daay == DateTime.Now.Date.Day).ToList();
+        }
+        public IQueryable<HOADON> load_listHD()
+        {
+            return dbContext.HOADONs.Select(hd => hd);
+        }
+        public HOADON loadHD_Last()
+        {
+            return dbContext.HOADONs.ToList().Last();
+        }
+        public bool TaoHoaDon(HOADON hd)
+        {
+            try
+            {
+                dbContext.HOADONs.InsertOnSubmit(hd);
+                dbContext.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //public IQueryable<View_HoaDon> load_listHD()
+        //{
+        //    return dbContext.View_HoaDons.Select(hd => hd);
+        //}
+        public HOADON getMaHDMoi()
+        {
+            return dbContext.HOADONs.OrderByDescending(hd => hd.MAHOADON).First();
+        }
+
+        DateTime day = new DateTime();
+
+
+        #region Thống kê
+        public IQueryable thongKeSpTheoThang(int pThang, int pNam)
+        {
+            var ans = from sp in dbContext.CHITIETSANPHAMs
+                      join cthd in dbContext.CHITIETHOADONs on sp.MACHITIETSP equals cthd.MACHITIETSP
+                      join hd in dbContext.HOADONs on cthd.MAHOADON equals hd.MAHOADON
+                      join spp in dbContext.SANPHAMs on sp.MASANPHAM equals spp.MASANPHAM
+                      where Convert.ToInt32(hd.NGAYTAO.Value.Month.ToString()) == pThang && Convert.ToInt32(hd.NGAYTAO.Value.Year.ToString()) == pNam
+                      group spp by spp.TENSANPHAM into g
+                      select new
+                      {
+                          TENSANPHAM = g.Key,
+                          SOLUONG = g.Count()
+                      };
+            return ans;
+        }
+
+        public IQueryable thongKeHoaDonTheoThang(int pThang, int pNam)
+        {
+            var ans = from sp in dbContext.CHITIETSANPHAMs
+                      join cthd in dbContext.CHITIETHOADONs on sp.MACHITIETSP equals cthd.MACHITIETSP
+                      join hd in dbContext.HOADONs on cthd.MAHOADON equals hd.MAHOADON
+                      where Convert.ToInt32(hd.NGAYTAO.Value.Month.ToString()) == pThang && Convert.ToInt32(hd.NGAYTAO.Value.Year.ToString()) == pNam
+                      join kh in dbContext.KHACHHANGs on hd.MAKHACHHANG equals kh.MAKHACHHANG
+                      select new
+                      {
+                          hd.MAHOADON,
+                          kh.TENKHACHHANG,
+                          hd.NGAYTAO,
+                          hd.THANHTIEN
+                      };
+            return ans;
+        }
+        #endregion
+    }
+}
---
+++ DAMH_NHOM1_2001180124_NGOTANLOC_QLSHOPTHOITRANG_ALL/4 . Source code chưa đóng gói/GUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using BLL_DAL;
+using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+using GUI.Cashier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using GUI.V_BaoHanh;
+namespace GUI
+{
+    static class Program
+    {
+        
+        public static FormLogin frmLogin;
+        public static string linkURL_Image = @"image\";
+        //public static string linkURL_SanPham = @"E:\Hoc\CNPM\GopDoAn\ShopThoiTrang\ShopThoiTrang\Nhom8_KDPM_PhanMemQuanLyShopQuanAo\Image\sanpham\";
+        //public static string linkURL_LoaiSP = @"E:\Hoc\CNPM\GopDoAn\ShopThoiTrang\ShopThoiTrang\Nhom8_KDPM_PhanMemQuanLyShopQuanAo\Image\loaisanpham\";
+        //public static string linkURL_KhachHang = @"E:\Hoc\CNPM\GopDoAn\ShopThoiTrang\ShopThoiTrang\Nhom8_KDPM_PhanMemQuanLyShopQuanAo\Image\KhachHang\";
+        //public static string linkURL_NhanVien = @"E:\Hoc\CNPM\GopDoAn\ShopThoiTrang\ShopThoiTrang\Nhom8_KDPM_PhanMemQuanLyShopQuanAo\Image\nhanvien\"; 
+        
+        public static FormTN formTN =null;
+        public static List<CHITIETHOADON> dsBaoHanh = new List<CHITIETHOADON>();
+        public static Cart dsPhieuNhap = new Cart();
+        public static Cart dsGH = new Cart();
+        public static int soLuong = Program.dsGH.tongSoLuong();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            frmLogin = new FormLogin();
+            Application.Run(frmLogin);
+        }
+    }
+}
